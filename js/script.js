eb39d0efa7c6ab57c368e1b8118684b2d60ac09d
Sam <samya.hussain@hotmail.fr>
2022-08-14 01:10:37 +0200

    second commit

--- js/script.js
+++ js/script.js
@@ -7,6 +7,62 @@ const rectangle6 = document.getElementById('rectangle6');
 const rectangles = [rectangle2, rectangle3, rectangle4, rectangle5, rectangle6];
 
 const perso = document.getElementById('perso');
+// récupérer la position du perso
+function getPosition(element) {
+    var xPosition = 0;
+    var yPosition = 0;
+
+    while (element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+    return {
+        x: xPosition,
+        y: yPosition
+    };
+}
+
+function animateX(){
+
+    perso.animate([
+        { top: '17px' },
+        // { transform: 'translateX(0px)' },
+        // { transform: 'translateX(100px)' },
+        { left: '176px' }
+        // { left: '176px' }
+        // keep position at the end of the animation
+    ], {
+        duration: 1000,
+        fill: 'forwards'
+    });
+} 
+
+function animateY(){
+    
+    perso.animate([
+        // { left: '176px' },
+        // { transform: 'translateY(0px)' },
+        // { transform: 'translateY(100px)' }
+        { bottom: '16px' },
+        { top: '20px' }
+    ], {
+        duration: 1000,
+        // stop at the end of the animation
+        fill: 'forwards'
+
+    });
+} 
+
+
+
+function renderAnimateX(){
+    animateX();
+    setTimeout(animateY, 1000);
+}
+renderAnimateX();
+
+
 
 
 function rectangle2on () {
@@ -14,22 +70,8 @@ function rectangle2on () {
     rectangle2.style.height = '50vh';
     rectangle2.style.border = 'none';
     rectangle2.style.transition = 'all 1s';
-    perso.animate([
-        {transform: 'translateX(0px)'},
-        {transform: 'translateX(100px)'},
-    ], {
-        duration: 1000,
-        iterations: InputEvent.INPUT_EVENT_LENGTH
-    });
-    perso.animate([
-        {transform: 'translateY(0px)'},
-        {transform: 'translateY(-110px)'},
-    ], {
-        duration: 1000,
-        iterations: InputEvent.INPUT_EVENT_LENGTH
-    });
-
-}
+    // put two animations in a array and call them one after the other
+}  
 
 function rectangle2Off (){
    rectangle2.style.height = '50vh';
@@ -37,8 +79,3 @@ function rectangle2Off (){
     rectangle2.style.backgroundColor = '#1d1d1d';
     rectangle2.style.transition = 'all 1s';
 }
-
-
-
-// anim perso
-
