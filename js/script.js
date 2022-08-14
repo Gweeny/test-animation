664b47404b723ca3f9d5daaf96afb2cfd7588630
Sam <samya.hussain@hotmail.fr>
2022-08-14 16:52:18 +0200

    first part working with keyframes

--- js/script.js
+++ js/script.js
@@ -7,63 +7,23 @@ const rectangle6 = document.getElementById('rectangle6');
 const rectangles = [rectangle2, rectangle3, rectangle4, rectangle5, rectangle6];
 
 const perso = document.getElementById('perso');
-// récupérer la position du perso
-function getPosition(element) {
-    var xPosition = 0;
-    var yPosition = 0;
-
-    while (element) {
-        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
-        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
-        element = element.offsetParent;
-    }
-    return {
-        x: xPosition,
-        y: yPosition
-    };
-}
-
-function animateX(){
-
-    perso.animate([
-        { top: '17px' },
-        // { transform: 'translateX(0px)' },
-        // { transform: 'translateX(100px)' },
-        { left: '176px' }
-        // { left: '176px' }
-        // keep position at the end of the animation
-    ], {
-        duration: 1000,
-        fill: 'forwards'
-    });
-} 
-
-function animateY(){
-    
-    perso.animate([
-        // { left: '176px' },
-        // { transform: 'translateY(0px)' },
-        // { transform: 'translateY(100px)' }
-        { bottom: '16px' },
-        { top: '20px' }
-    ], {
-        duration: 1000,
-        // stop at the end of the animation
-        fill: 'forwards'
-
-    });
-} 
-
-
-
-function renderAnimateX(){
-    animateX();
-    setTimeout(animateY, 1000);
-}
-renderAnimateX();
-
-
-
+// detect if the mouse is over the rectangle2 is on and console log it
+// if (rectangle2.addEventListener('mouseover', rectangle2on)) {
+    //     console.log('rectangle2 is on');
+    // }elseif (rectangle5.addEventListener('mouseover', rectangle2Off)) {
+        //     perso.classList.add('perso-win2');
+        //     console.log('rectangle5 is on');
+        // }
+rectangle2.addEventListener('mouseover', function () {
+    perso.classList.add('perso-win1');
+    perso.classList.remove('perso-loose1');
+    console.log('rectangle2 is on');    
+} );
+rectangle2.addEventListener('mouseout', function () {
+    perso.classList.remove('perso-win1');
+    perso.classList.add('perso-loose1');
+    console.log('rectangle2 is off');
+} );
 
 function rectangle2on () {
     rectangle2.style.backgroundColor = 'rgb(203, 172, 113)';
@@ -78,4 +38,29 @@ function rectangle2Off (){
     rectangle2.style.border = '1px dotted black';
     rectangle2.style.backgroundColor = '#1d1d1d';
     rectangle2.style.transition = 'all 1s';
+}
+
+function rectangle3on () {
+    rectangle3.style.backgroundColor = 'rgb(203, 172, 113)';
+    rectangle3.style.height = '30vh';
+    rectangle3.style.border = 'none';
+    rectangle3.style.transition = 'all 1s';
+}
+function rectangle3Off (){
+    rectangle3.style.height = '30vh';
+    rectangle3.style.border = '1px dotted black';
+    rectangle3.style.backgroundColor = '#1d1d1d';
+    rectangle3.style.transition = 'all 1s';
+}
+function rectangle4on () {
+    rectangle4.style.backgroundColor = 'rgb(203, 172, 113)';
+    rectangle4.style.height = '15vh';
+    rectangle4.style.border = 'none';
+    rectangle4.style.transition = 'all 1s';
+}
+function rectangle4Off (){
+    rectangle4.style.height = '15vh';
+    rectangle4.style.border = '1px dotted black';
+    rectangle4.style.backgroundColor = '#1d1d1d';
+    rectangle4.style.transition = 'all 1s';
 }
